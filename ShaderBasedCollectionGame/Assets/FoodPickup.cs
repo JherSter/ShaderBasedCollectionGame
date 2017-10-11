ed6e31d0164823a7ba074b61127b011da96c5e5c
jeroen avis <jeroenavis123@gmail.com>
2017-10-11 12:19:41 +0200

    Done

---
+++ ShaderBasedCollectionGame/Assets/FoodPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodPickup : MonoBehaviour {
+	[SerializeField] Material _Fat;
+	private float _FatLvL = 0f;
+	void Start(){
+	 _FatLvL = 0f;
+	}
+	void Update(){
+		_FatLvL -= 0.001f;
+		_Fat.SetFloat ("_Fat", _FatLvL);
+	}
+	void OnTriggerEnter(Collider other) {
+		Destroy(gameObject);
+		_FatLvL = 6f;
+		_Fat.SetFloat ("_Fat", _FatLvL);
+
+	}
+}
+
